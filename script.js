5dee9c54e7b8f4a0425f334db343893283f8f9d0
abylaikhannnn <148153320+abylaikhannnn@users.noreply.github.com>
2025-02-03 10:44:42 +0500

    Update script.js
    Signed-off-by: abylaikhannnn <148153320+abylaikhannnn@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.getElementById("myButton");
 button.addEventListener("click",handleClick);
 function handleClick() {
     //Код, который будет выполняться при нажатии на кнопку
-    alert("Расскажи мне что-нибудь забавное.");
+    alert("Ты такая красивая.");
 }
