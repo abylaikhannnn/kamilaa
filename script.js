8b4f7efae0a5957fb8887da6c2fe9cfa45a810f5
abylaikhannnn <148153320+abylaikhannnn@users.noreply.github.com>
2025-02-03 10:22:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton");
+button.addEventListener("click",handleClick);
+function handleClick() {
+    //Код, который будет выполняться при нажатии на кнопку
+    alert("Расскажи мне что-нибудь забавное.");
+}
